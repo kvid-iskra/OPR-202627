a40d9b751a311405ce5ad91810ee6a9dde2a634b
kvid-iskra <vid.sola@outlook.com>
2026-09-14 10:53:01 +0000

    14.09.2026

--- Vaje/OpenMeteo.py
+++ Vaje/OpenMeteo.py
@@ -5,4 +5,10 @@ def trenutna_temp(lat, lon):
     call = requests.get(base_url).json()
     print(call["current"]["temperature_2m"])
 
-trenutna_temp(45.12, 14.5)
+def temparatura_7dni(lat, lon):
+    base_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min"
+    call = requests.get(base_url).json()
+    print(call["daily"]["time"], call["daily"]["temperature_2m_max"])
+
+#trenutna_temp(46.23887, 14.35561)
+temparatura_7dni(46.23887, 14.35561)
