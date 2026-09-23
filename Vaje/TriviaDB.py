4c49fd6df8b3cb25f7d738878e8fefc2c8f0e888
kvid-iskra <vid.sola@outlook.com>
2026-09-23 08:32:30 +0000

    Add trivia quiz functionality with API integration

---
+++ Vaje/TriviaDB.py
@@ -0,0 +1,51 @@
+import requests
+from pprint import pprint
+import html
+import random
+ 
+vpr = int(input("Koliko vprašanj želiš? "))
+url = f"https://opentdb.com/api.php?amount={vpr}&type=multiple"
+klic = requests.get(url).json()
+ 
+vprasanja = klic["results"]
+
+tocke = 0
+ 
+for v in vprasanja:
+    print("-"*80)
+    #pprint(v)
+    #print("-"*80)
+ 
+    print(html.unescape(v["question"]))  #odstrani HTML znake
+    pravilni_odgovor = v["correct_answer"]
+    napacni_odgovori = v["incorrect_answers"]
+    pravilen_odgovor_seznam = [pravilni_odgovor]
+    vsi_odogovori = napacni_odgovori + pravilen_odgovor_seznam
+    random.shuffle(vsi_odogovori)
+ 
+    prav = pravilni_odgovor
+    odgovori = [vsi_odogovori[0], vsi_odogovori[1], vsi_odogovori[2], vsi_odogovori[3]]
+    for i, o in enumerate(odgovori):
+        print(f"{i+1} - {o}")
+    odgovor = int(input("Odgovor: "))
+    if prav == odgovori[odgovor-1]:
+        print("Odogovor je pravilen!")
+        tocke += 1
+    else:
+        print("Odgovor je napačen!")
+
+print("Osvojili ste {tocke} točk")
+
+"""
+VPRAŠANJE?
+ 
+ODGOVOR 1
+ODGOVOR 2
+ODGOVOR 3
+ODGOVOR 4
+ 
+Odgovor: ??
+ 
+Pravilno/napačno
+BONUS: število točk
+"""
